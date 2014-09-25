6cd1310c42bffbda4c1b6dba13c3c06f91af5b0f
Zniper <me.zniper@gmail.com>
2014-09-25 10:52:52 +0700

    Switch to use django-scraper

--- spider/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from spider import models
-
-admin.site.register(models.Source)
-admin.site.register(models.ContentType)
-admin.site.register(models.LocalContent)
-admin.site.register(models.WordSet)
